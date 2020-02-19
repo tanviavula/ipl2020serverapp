ab475b6f7d75c5cdeaad653e9394af469d641035
unknown <lakshman.avula@outlook.com>
2020-02-19 22:30:02 +0530

    Changed library scope

--- src/main/java/com/nubes/ipl2020/SwaggerConfig.java
+++ src/main/java/com/nubes/ipl2020/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.nubes.ipl2020;
 
 import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
