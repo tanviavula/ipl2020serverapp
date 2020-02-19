32cc89eccb1a74af9383b53bab6f2dcf7f0ca771
unknown <lakshman.avula@outlook.com>
2020-02-20 00:02:31 +0530

    Changed logic

--- src/main/java/com/nubes/ipl2020/Ipl2020Application.java
+++ src/main/java/com/nubes/ipl2020/Ipl2020Application.java
@@ -1,9 +1,18 @@
 package com.nubes.ipl2020;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Ipl2020Application {
 
 	public static void main(String[] args) {
@@ -11,4 +20,14 @@ public class Ipl2020Application {
 		application.run(args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.nubes.ipl2020")).build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("IPL-2020 Statistics")
+				.description("\"Spring Boot REST API for IPL-2020 Statistics\"").version("1.0.0").build();
+	}
 }
--- src/main/java/com/nubes/ipl2020/SwaggerConfig.java
+++ src/main/java/com/nubes/ipl2020/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.nubes.ipl2020;
+
 import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,31 +14,23 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nubes.ipl2020.web"))
-                .paths(regex("/ipl2020.*"))
-                .build()
-                .apiInfo(metaData());
-    }
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("IPL-2020 Statistics")
-                .description("\"Spring Boot REST API for IPL-2020 Statistics\"")
-                .version("1.0.0")
-                .build();
-    }
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
 
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+//	@Bean
+//	public Docket productApi() {
+//		return new Docket(DocumentationType.SWAGGER_2).select()
+//				.apis(RequestHandlerSelectors.basePackage("com.nubes.ipl2020.web")).paths(regex("/ipl2020.*")).build()
+//				.apiInfo(metaData());
+//	}
+//
+//	private ApiInfo metaData() {
+//		return new ApiInfoBuilder().title("IPL-2020 Statistics")
+//				.description("\"Spring Boot REST API for IPL-2020 Statistics\"").version("1.0.0").build();
+//	}
+//
+//	@Override
+//	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//	}
 }
